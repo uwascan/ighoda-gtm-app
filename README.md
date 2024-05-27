2e66be77966ef547de8394a8c4e9482523aae05c
Emovon Uwadia <uwascanix@gmail.com>
2024-05-27 16:38:41 +0100

    Ighodaro Victor Igbins Deno GTM App

---
+++ README.md
@@ -0,0 +1,42 @@
+# Google Analytics 4 Tutorial Series
+
+This repository contains the code used in the demo website for the
+[Google Analytics 4 (GA4) Tutorial Series](https://www.youtube.com/watch?v=oJx9DpXtmAE&list=PLI5YfMzCfRtZ4bHJJDl_IJejxMwZFiBwz)
+on YouTube. You can find the code for each video in the [/tutorials](/tutorials)
+directory.
+
+The current code is found in the [/src](/src) directory, which contains all the
+changes made in each video up to the most recent video.
+
+## Run the current version of the website
+
+You must install [Deno](https://deno.land) to run the website code.
+
+Once you install Deno, run the local web server with the following command:
+
+```
+deno task start
+```
+
+Then, open <http://localhost> in your web browser.
+
+By default, the server listens on port 80. If that port is in use or is
+otherwise unavailable, override the port using the `--port` argument.  For
+example, you could run the server on port 8000 as follows:
+
+```
+deno task start --port 8000
+```
+
+Then, open <http://localhost:8000> in your web browser.
+
+# Join our community
+
+💬 [Join](https://discord.gg/65mah7ZZsG) the official GA Discord server\
+📝 [Sign up](https://groups.google.com/g/google-analytics-developer-newsletter)
+for the Google Analytics Developer Newsletter\
+📄 [Explore](https://developers.google.com/analytics/) GA4 developer
+documentation
+
+
+GTM-PT2CSBKW
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { configure, renderFile } from "https://deno.land/x/eta@v1.11.0/mod.ts";
+import { parse } from "https://deno.land/std@0.207.0/flags/mod.ts";
+
+const __dirname = new URL(".", import.meta.url).pathname;  
+//const __dirname = "C:\\Users\\ADMIN\\Desktop\\GANALYTICS\\App\\src\\";
+
+const viewPath = [
+  `${__dirname}/public`,
+  `${__dirname}/public/partials`,
+  `${__dirname}/public/layouts`,
+];
+
+configure({ views: viewPath });
+const flags = parse(Deno.args, {
+  string: [ "port" ],
+  default: { port: 80 },
+});
+
+const server = Deno.listen({ port: flags.port });
+
+console.log("File server running on http://localhost:" + flags.port + "/");
+
+for await (const conn of server) {
+  handleHttp(conn).catch(console.error);
+}
+
+async function handleHttp(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const requestEvent of httpConn) {
+    const url = new URL(requestEvent.request.url);
+    let filepath = decodeURIComponent(url.pathname);
+    if (filepath === "/") {
+      filepath = "index.eta";
+    } else if (filepath.toLocaleLowerCase().indexOf(".") <= 0) {
+      filepath = `${filepath}.eta`;
+    }
+
+    let file;
+    let response;
+    try {
+      console.log('DIR-NMAE', __dirname);
+      console.log('FILE-PATH', filepath);
+      if (filepath.indexOf(".eta") > 0) {
+        response = new Response(await renderFile(filepath, {}), {
+          headers: { "content-type": "text/html" },
+        });
+      } else {
+        file = await Deno.open(__dirname + "public" + filepath, {
+          read: true,
+        });
+        const readableStream = file.readable;
+        response = new Response(readableStream);
+      }
+    } catch (e) {
+      console.error(e);
+      response = new Response("404 Not Found", { status: 404 });
+      return;
+    }
+
+    await requestEvent.respondWith(response);
+  }
+}
